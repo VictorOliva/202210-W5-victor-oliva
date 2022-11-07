e0240f5a664690b6b50c9b84d6982ad428759eda
victor <victor202@gmail.com>
2022-11-07 21:10:08 +0100

    Length funtion done and test

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,7 @@
-export const add = (a, b) => {
-    return a + b;
+export const arrayLength = (array) => {
+    let counter = 0;
+    for (const i in array) {
+        counter = Number(i) + 1;
+    }
+    return counter;
 };
